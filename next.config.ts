5352cddc640da8f8878e92d145405597072215c9
SHRUSTI JIRGI <shrustijeon@gmail.com>
2025-12-07 15:43:35 +0530

    Remove invalid eslint config for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  // Add this eslint block to ignore errors during Vercel build
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   // You can add other Next.js config options here later
 };
 
